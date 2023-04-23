5c83887465df949ab9513c02b4eaecd59036d26d
soso01 <soso01dev@gmail.com>
2023-04-24 00:16:24 +0900

    feat: 이미지 에셋 추가

---
+++ @apps/client/src/constants/image.ts
@@ -0,0 +1,8 @@
+const base = '../../assets/';
+
+export const ImageAssets = {
+  Login: {
+    Google: require(base + 'login/google.png'),
+    Apple: require(base + 'login/apple.png'),
+  },
+};
--- @apps/client/src/screens/Auth/LoginButtons.tsx
+++ @apps/client/src/screens/Auth/LoginButtons.tsx
@@ -1,18 +1,19 @@
 import styled from '@emotion/native';
+import { Image } from 'expo-image';
+
+import { ImageAssets } from '@/constants';
 
 export const LoginButtons = () => {
   return (
     <S.Wrap>
       <S.Button>
+        <S.Icon source={ImageAssets.Login.Google} />
         <S.ButtonText>Sign In With Google</S.ButtonText>
       </S.Button>
 
       <S.Button>
+        <S.Icon source={ImageAssets.Login.Apple} />
         <S.ButtonText>Sign In With Apple</S.ButtonText>
-      </S.Button>
-
-      <S.Button>
-        <S.ButtonText>Sign In With Guest</S.ButtonText>
       </S.Button>
     </S.Wrap>
   );
@@ -24,6 +25,8 @@ const S = {
   `,
   Button: styled.TouchableOpacity`
     display: flex;
+    flex-direction: row;
+    justify-content: center;
     align-items: center;
     margin: 0 32px;
     padding: 18px;
@@ -36,6 +39,12 @@ const S = {
     }
   `,
   ButtonText: styled.Text`
+    font-size: 16px;
     color: ${({ theme }) => theme.color.primaryText};
+  `,
+  Icon: styled(Image)`
+    width: 24px;
+    height: 24px;
+    margin-right: 18px;
   `,
 };
