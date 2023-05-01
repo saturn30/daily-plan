3aefd5875733b17fb4025b5562da56247417fc62
soso01 <soso01dev@gmail.com>
2023-05-01 19:32:52 +0900

    feat: 스케쥴 컨테이너 추가

---
+++ @apps/client/src/features/schedule/container/ScheduleContainer.tsx
@@ -0,0 +1,21 @@
+import styled from '@emotion/native';
+
+export const ScheduleContainer = () => {
+  return (
+    <S.Wrap>
+      {Array(100)
+        .fill('AAA')
+        .map((value, index) => (
+          <S.Text key={`value${index}`}>{value}</S.Text>
+        ))}
+    </S.Wrap>
+  );
+};
+
+const S = {
+  Wrap: styled.ScrollView`
+    flex: 1;
+    margin-top: 8px;
+  `,
+  Text: styled.Text``,
+};
--- @apps/client/src/screens/Home/Home.tsx
+++ @apps/client/src/screens/Home/Home.tsx
@@ -1,13 +1,12 @@
-import { Text } from 'react-native';
-
 import { Layout } from '@/common/components/Layout';
 import { SmallCalendarContainer } from '@/features/calendar';
+import { ScheduleContainer } from '@/features/schedule';
 
 export const HomeScreen = () => {
   return (
     <Layout>
       <SmallCalendarContainer />
-      <Text>Home</Text>
+      <ScheduleContainer />
     </Layout>
   );
 };
