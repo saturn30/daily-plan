440597e0965e4d673a66a06fe44cdc7d16866b1f
soso01 <soso01dev@gmail.com>
2023-05-06 01:04:58 +0900

    refactor: y값 드래그만 허용

--- @apps/client/src/features/schedule/components/ScheduleRow/useScheduleDrag.ts
+++ @apps/client/src/features/schedule/components/ScheduleRow/useScheduleDrag.ts
@@ -11,7 +11,6 @@ import {
 export const useScheduleDrag = () => {
   const isAnimated = useRef(false);
   const [isMoving, setIsMoving] = useState(false);
-  const translateX = useSharedValue(0);
   const translateY = useSharedValue(0);
 
   const stopMoving = () => {
@@ -29,17 +28,14 @@ export const useScheduleDrag = () => {
     { translateX: number; translateY: number }
   >({
     onStart: (event, context) => {
-      context.translateX = translateX.value;
       context.translateY = translateY.value;
       runOnJS(setIsMoving)(true);
       runOnJS(setRef)(true);
     },
     onActive: (event, context) => {
-      translateX.value = event.translationX + context.translateX;
       translateY.value = event.translationY + context.translateY;
     },
     onEnd: (event, context) => {
-      translateX.value = withSpring(0, { mass: 0.7 });
       translateY.value = withSpring(0, { mass: 0.7 });
       runOnJS(setRef)(false);
       runOnJS(stopMoving)();
@@ -49,9 +45,6 @@ export const useScheduleDrag = () => {
   const rStyle = useAnimatedStyle(() => {
     return {
       transform: [
-        {
-          translateX: translateX.value,
-        },
         {
           translateY: translateY.value,
         },
